bf2b586126d436c091b82c494d03f096146023bb
armaz avaliani <avaliani1232@gmail.com>
2022-04-07 06:49:36 +0400

    without a link of nav

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ VanillaTilt.init(document.querySelectorAll(".services-card"), {
     max: 25,
     speed: 400
 });
+const primaryNav = document.querySelector('.primary-navigation'); 
+const navToggle = document.querySelector('.mobile-nav-toggle');
+navToggle.addEventListener('click', () =>{
+    const visibility = primaryNav.getAttribute('data-visible')
+    if(visibility === 'false'){
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    }else if(visibility === 'true'){
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+
+})
